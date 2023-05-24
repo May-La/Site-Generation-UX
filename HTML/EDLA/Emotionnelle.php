4710dacc70757ce1bdc595af0116c01f8adad487
mlageste <lageste.maylis@gmail.com>
2023-05-24 15:45:14 +0200

    Emo Part 1 faite

--- HTML/EDLA/Emotionnelle.php
+++ HTML/EDLA/Emotionnelle.php
@@ -5,6 +5,7 @@
 <?php require_once '../../includes/navbar.php'; ?>
 <body>
     <h2>L'UX ou l'expérience utilisateur</h2>
+    <br>
     <p>
         L’UX ou l’expérience utilisateur est “communément décrite dans la littérature comme la qualité globale de l’interaction entre un utilisateur et un système interactif” (C. Lallemand, 2015). <br>
         Les échelles UX se présentent sous la forme de questionnaires standardisés et servent donc à recueillir l’avis général de l’utilisateur sur un système. Celles-ci sont divisées en plusieurs catégories d’évaluations. 
@@ -13,8 +14,10 @@
         En effet, AttrakDiff et UEQ sont basées sur le modèle de Hassenzahl (2003).
     </p> 
     <br>
-    <a href = "https://carinelallemand.files.wordpress.com/2014/11/modc3a8le-ux-hassenzahl.png"><img src = "../../IMG/hassenzahlModel.png" id = "Fig1" alt = "Modèle de Hassenzahl"></a>
-    <h5>Figure 1. Modèle de Hassenzahl</h5>
+    <div class = "text-center">
+        <a href = "https://carinelallemand.files.wordpress.com/2014/11/modc3a8le-ux-hassenzahl.png"><img src = "../../IMG/hassenzahlModel.png" id = "Fig1" alt = "Modèle de Hassenzahl"></a>
+        <h5>Figure 1. Modèle de Hassenzahl</h5>
+    </div>
     <br>
     <p>
         D’après le modèle de Hassenzahl <a href = "#Fig1">(Figure 1)</a>, l’expérience utilisateur est composée de la perspective du concepteur et de celle de l’utilisateur. 
@@ -22,16 +25,31 @@
         Puisque les perspectives sont subjectives, les caractéristiques peuvent différer.
     </p>
     <br>
-    <a href = "https://carinelallemand.files.wordpress.com/2014/11/caractc3a8res-de-produits-hassenzahl.png"><img src = "../../IMG/ModeleCarac.png" id = "Fig2" alt = "Carctéristiques du modèle"></a>
-    <h5>Figure 2. Représentation des caractéristiques du modèle de Hassenzahl</h5>
+    <div class = "text-center">
+        <a href = "https://carinelallemand.files.wordpress.com/2014/11/caractc3a8res-de-produits-hassenzahl.png"><img src = "../../IMG/ModeleCarac.png" id = "Fig2" alt = "Carctéristiques du modèle"></a>
+        <h5>Figure 2. Représentation des caractéristiques du modèle de Hassenzahl</h5>
+    </div>
     <br>
     <p>
         Le modèle propose 4 grandes caractéristiques: les attributs pragmatiques, les attributs hédoniques, le produit ACT et le produit SELF <a href = "#Fig2">(Figure 2)</a>. <br> <br>
         Les attributs pragmatiques correspondent à l’utilité, l’utilisabilité du produit et la capacité à réaliser des tâches. Les attributs hédoniques correspondent à la stimulation, l’identification et l’évocation. 
         Ce sont ces derniers qui vont déterminer l’attractivité du produit ainsi que les émotions provoquées lors de l’utilisation. <br>
         Un produit ACT va être centré sur l’utilisabilité, l’efficacité. Il va produire de la satisfaction chez l’utilisateur. Ainsi, il aura des attributs pragmatiques forts et des attributs hédoniques faibles. <br>
-        Un produit SELF va être centré sur l’utilisateur, sa personne et ses ressentis. Il va produire du plaisir chez l’utilisateur. Ainsi, il aura des attributs pragmatiques faibles et des attributs hédoniques forts. <br>
+        Un produit SELF va être centré sur l’utilisateur, sa personne et ses ressentis. Il va produire du plaisir chez l’utilisateur. Ainsi, il aura des attributs pragmatiques faibles et des attributs hédoniques forts. <br> <br>
+        MeCue est basée quant à elle sur le Modèle UX de Thüring et Mahlke (2007).
     </p>
+    <br>
+    <div class = "text-center">
+        <a href = "#"><img src = "../../IMG/MahlkeModel.png" id = "Fig3" alt = "Modèle de Mahlke et Thuring"></a>
+        <h5>Figure 3. Modèle de Mahlke et Thüring</h5>
+    </div>
+    <br>
+    <p>
+        D’après ce modèle (Figure 12), l’expérience utilisateur est déterminée par trois éléments: la perception des qualités instrumentales, la perception des qualités non instrumentales et les réponses émotionnelles. <br>
+        Les qualités instrumentales correspondent à l’utilité, l’utilisabilité et l’efficacité du système. Les qualités non-instrumentales correspondent à l’attractivité, la fréquence d’usage et les conséquences morales que peuvent avoir le système sur l’utilisateur. 
+        Les réponses émotionnelles sont influencées par ces deux éléments et permettent de connaître l’état moral de l’utilisateur lors des interactions avec le système.
+    </p>
+
 
 </body>
 <?php require_once '../../includes/footer.php' ?>
