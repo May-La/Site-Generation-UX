aecdd329f8e2a01d2ca217a4ba9e8099b068df88
ChloeMonv <cmonvoisi003@ensc.fr>
2023-05-25 13:23:14 +0200

    Versions adpatation

--- HTML/Adaptations.php
+++ HTML/Adaptations.php
@@ -5,9 +5,14 @@
 <body>
 
 
-
-
-
+<img class = "Nous" src="../IMG/meCue.png" alt="Echelle meCue originelle">
+<p class = "middle">Echelle MeCue originelle</p>
+<img class = "Nous" src="../IMG/Adaptation meCue V2.svg" alt="Echelle meCue Design Adapté ">
+<p class = "middle">Echelle MeCue avec le design adapté</p>
+<img class = "Nous" src="../IMG/Adaptation meCue V3 sur 2 pages-1-1.png" alt="Echelle meCue Texte Adapté ">
+<p class = "middle">Echele MeCue avec le texte adapté</p>
+<img class = "Nous" src="../IMG/Adpatation meCue V2R.png" alt="Echelle meCue Texte Adapté ">
+<p class = "middle">Echele MeCue adaptée apres les tests</p>
 </body>
 <?php require_once '../includes/footer.php' ?>
 </html>
