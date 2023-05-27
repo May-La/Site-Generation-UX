a730d29f5a9f05108e615abb6ec3c6b49dfd0221
mlageste <lageste.maylis@gmail.com>
2023-05-28 01:46:38 +0200

    Plein d'ajouts

--- HTML/EDLA.php
+++ HTML/EDLA.php
@@ -7,7 +7,7 @@
     <H2>Etat de l'art</H2>
 
     <p>Nous avons commencés par un etat de l'art afin de rechercher les échelles les plus utilisées et de choisir celles que nous voulions adapter</p>
-    <a href="../GenerationUX_Etat-de-l-art.pdf">Lien vers l'etat de l'art complet en PDF</a>
+    <a href="../PDF/GenerationUX_Etat-de-l-art.pdf">Lien vers l'etat de l'art complet en PDF</a>
 
 </body>
 <?php require_once '../includes/footer.php' ?>
--- HTML/UX.php
+++ HTML/UX.php
@@ -6,7 +6,7 @@
 <body>
 
     <br>
-    <h2>L'UX ou l'expérience utilisateur</h2>
+    <h2 id = "$1">L'UX ou l'expérience utilisateur</h2>
     <br>
     <p>
         L’UX ou l’expérience utilisateur est “communément décrite dans la littérature comme la qualité globale de l’interaction entre un utilisateur et un système interactif” (C. Lallemand, 2015). <br>
@@ -17,7 +17,7 @@
     </p> 
     <br>
     <div class = "text-center">
-        <a href = "https://carinelallemand.files.wordpress.com/2014/11/modc3a8le-ux-hassenzahl.png"><img src = "../IMG/hassenzahlModel.png" id = "Fig1" alt = "Modèle de Hassenzahl"></a>
+        <a href = "https://carinelallemand.files.wordpress.com/2014/11/modc3a8le-ux-hassenzahl.png"><img src = "../IMG/hassenzahlModel.png" id = "Fig1" alt = "Modèle de Hassenzahl" WIDTH="50%"></a>
         <h5>Figure 1. Modèle de Hassenzahl</h5>
     </div>
     <br>
@@ -28,7 +28,7 @@
     </p>
     <br>
     <div class = "text-center">
-        <a href = "https://carinelallemand.files.wordpress.com/2014/11/caractc3a8res-de-produits-hassenzahl.png"><img src = "../IMG/ModeleCarac.png" id = "Fig2" alt = "Carctéristiques du modèle"></a>
+        <a href = "https://carinelallemand.files.wordpress.com/2014/11/caractc3a8res-de-produits-hassenzahl.png"><img src = "../IMG/ModeleCarac.png" id = "Fig2" alt = "Caractéristiques du modèle" WIDTH="20%"></a>
         <h5>Figure 2. Représentation des caractéristiques du modèle de Hassenzahl</h5>
     </div>
     <br>
@@ -36,21 +36,122 @@
         Le modèle propose 4 grandes caractéristiques: les attributs pragmatiques, les attributs hédoniques, le produit ACT et le produit SELF <a href = "#Fig2">(Figure 2)</a>. <br> <br>
         Les attributs pragmatiques correspondent à l’utilité, l’utilisabilité du produit et la capacité à réaliser des tâches. Les attributs hédoniques correspondent à la stimulation, l’identification et l’évocation. 
         Ce sont ces derniers qui vont déterminer l’attractivité du produit ainsi que les émotions provoquées lors de l’utilisation. <br>
-        Un produit ACT va être centré sur l’utilisabilité, l’efficacité. Il va produire de la satisfaction chez l’utilisateur. Ainsi, il aura des attributs pragmatiques forts et des attributs hédoniques faibles. <br>
-        Un produit SELF va être centré sur l’utilisateur, sa personne et ses ressentis. Il va produire du plaisir chez l’utilisateur. Ainsi, il aura des attributs pragmatiques faibles et des attributs hédoniques forts. <br> <br>
-        MeCue est basée quant à elle sur le Modèle UX de Thüring et Mahlke (2007).
+        Un produit ACT va être centré sur l’utilisabilité, l’efficacité. Il va produire de la satisfaction chez l’utilisateur. Ainsi, il aura des attributs pragmatiques forts et des attributs hédoniques faibles.
+        Un produit SELF va être centré sur l’utilisateur, sa personne et ses ressentis. Il va produire du plaisir chez l’utilisateur. Ainsi, il aura des attributs pragmatiques faibles et des attributs hédoniques forts.
+    </p>
+    <p>
+    MeCue est basée quant à elle sur le Modèle UX de Thüring et Mahlke (2007).
     </p>
     <br>
     <div class = "text-center">
-        <a href = "#"><img src = "../IMG/MahlkeModel.png" id = "Fig3" alt = "Modèle de Mahlke et Thuring"></a>
+        <a href = "#"><img src = "../IMG/MahlkeModel.png" id = "Fig3" alt = "Modèle de Mahlke et Thuring" WIDTH="70%"></a>
         <h5>Figure 3. Modèle de Mahlke et Thüring</h5>
     </div>
     <br>
     <p>
-        D’après ce modèle (Figure 12), l’expérience utilisateur est déterminée par trois éléments: la perception des qualités instrumentales, la perception des qualités non instrumentales et les réponses émotionnelles. <br>
+        D’après ce modèle <a href = "#Fig3">(Figure 3)</a>, l’expérience utilisateur est déterminée par trois éléments: la perception des qualités instrumentales, la perception des qualités non instrumentales et les réponses émotionnelles.
         Les qualités instrumentales correspondent à l’utilité, l’utilisabilité et l’efficacité du système. Les qualités non-instrumentales correspondent à l’attractivité, la fréquence d’usage et les conséquences morales que peuvent avoir le système sur l’utilisateur. 
         Les réponses émotionnelles sont influencées par ces deux éléments et permettent de connaître l’état moral de l’utilisateur lors des interactions avec le système.
     </p>
+    <hr ALIGN=CENTER WIDTH="50%"> 
+    <br>
+
+    <h2 id = "$2">Exemples d'échelles UX</h2> 
+    <br>
+    <h3 id = "$2.1">I. AttrakDiff</h3>
+    <br>
+    <p>
+        AttrakDiff est une échelle mise au point en 2003 par Hassenzahl, Burmester et Koller, des chercheurs allemands. 
+        Elle a été traduite en français en 2014 par C. Lallemand <a href = "https://lagrandeourse.design/wp-content/uploads/2021/07/Evaluation-questionnaire-Attrakdiff-Activites-UX.pdf">(Echelle complète ici)</a>. 
+        Très utilisée encore aujourd’hui, elle permet d’évaluer la qualité de l’expérience utilisateur avec un système interactif en 4 critères de 7 items. 
+        Ces critères sont la qualité pragmatique, la qualité hédonique-simulation, la qualité hédonique-identité et l’attractivité globale. 
+    </p>
+    <p>
+        La qualité pragmatique correspond à l’utilisabilité du système et indique à quel point il permet aux utilisateurs d’atteindre leurs buts (dans le sens de réaliser une tâche). 
+        La qualité hédonique-simulation permet de mesurer la stimulation sensorielle et intellectuelle entraînée par le système. 
+        La qualité hédonique-identité évalue dans quelle mesure le système permet à l’utilisateur de s’identifier à lui.
+        Enfin, l’attractivité globale décrit la valeur globale du système à partir des qualités pragmatiques et hédoniques.
+    </p>
+    <p>
+        Chaque item est composé de deux antonymes séparés par 7 cercles, représentant l’accord ou le désaccord de l’utilisateur avec les qualifications proposées. 
+        L’utilisateur cochera alors le cercle qu’il jugera le plus représentatif de son avis. 
+        Les antonymes peuvent être des adjectifs ou des verbes conjugués à la troisième personne du singulier au présent comme nous pouvons le voir dans l’extrait ci-dessous <a href = "#Fig4">(Figure 4)</a>.
+    </p>
+    <div class = "text-center">
+        <a href = "https://carinelallemand.files.wordpress.com/2015/09/version-franc3a7aise-attrakdiff_lallemand_2015.pdf"><img src = "../IMG/ExtraitAttrakDiff.png" id = "Fig4" alt = "Module 1 AttrakDiff" WIDTH="40%"></a>
+        <br>
+        <br>
+        <h5>Figure 4. Extrait de l'échelle AttrakDiff</h5>
+    </div>
+    <p>
+        De manière générale, l’échelle est très complète et permet de recueillir un grand nombre de données intéressantes par rapport à l’utilisateur. 
+        Cependant, pour des enfants de 7 à 14 ans, le vocabulaire peut être complexe voire difficilement accessible et l’échelle est longue à compléter. 
+        Pour pallier au deuxième point, une version courte du questionnaire existe, composée de dix items au vocabulaire plus accessible <a href = "#Fig5">(Figure 5)</a>. 
+        Cette version a été mise au point en 2019 par Antonio Giardi dans un article scientifique intitulé “Evaluate children's User experience with AttrakDiff method: USiena”. 
+        Néanmoins, elle n’existe pas en français.
+    </p>
+    <div class = "text-center">
+        <a href = "https://shorturl.at/bsvE2"><img src = "../IMG/AttrakDiffCourt.png" id = "Fig5" alt = "AttrakDiff raccourci" WIDTH="40%"></a>
+        <br>
+        <br>
+        <h5>Figure 5. AttrakDiff version courte</h5>
+    </div>
+    <p>
+        Également, pour faciliter la compréhension de cette tranche d’âge, le chercheur italien Antonio Giardi a mis au point une aide visuelle en 2019 <a href = "#Fig6">(Figure 6)</a>. 
+        De la même manière, cette aide n’est disponible qu’en anglais.
+    </p>
+    <div class = "text-center">
+        <a href = "https://www.researchgate.net/figure/Questionnaire-AttrakDiff-shortened-modified-University-of-Siena_fig1_340950602"><img src = "../IMG/SmileysAttrakDiff.png" id = "Fig6" alt = "Aide à la compréhension d'AttrakDiff" WIDTH="40%"></a>
+        <br>
+        <br>
+        <h5>Figure 6. Aide à la compréhension du questionnaire</h5>
+    </div>
+    <hr ALIGN=CENTER WIDTH="20%"> 
+    <br>
+    <h3 id = "$2.2">II. UEQ (User Experience Questionnaire)</h3>
+    <br>
+    <p>
+        Mise au point en 2008 par Laugwitz, Schrepp et Held, des chercheurs allemands, en anglais et allemand. Elle a ensuite été traduite en français par Alain Tuor <a href = "../PDF/UEQ_French_v3.pdf">(Échelle complète ici)</a>. 
+        Très populaire, l'UEQ permet d’évaluer n’importe quel système interactif en 26 items. Ceux-ci sont divisés en six critères: attractivité, accessibilité, efficacité, fiabilité, stimulation et le caractère novateur du système. 
+        Il existe également une version courte composée de 8 items seulement.
+    </p>
+    <p>
+        Cette échelle fonctionne de la même manière que l’AttrakDiff mais permet d’évaluer plus de critères différents. 
+        Le vocabulaire est également plus accessible pour la majorité des items.
+    </p>
+    <div class = "text-center">
+        <img src = "../IMG/ExtraitUEQ.png" id = "Fig7" alt = "Extrait UEQ" WIDTH="50%">
+        <br>
+        <br>
+        <h5>Figure 7. Extrait de l'UEQ</h5>
+    </div>
+    <p>
+        La version courte se concentre sur l’accessibilité et l’efficacité du produit, délaissant ainsi les autres critères d’évaluation.
+    </p>
+    <p>
+        En conclusion, également très complète, cette échelle permet de recueillir des données sur un grand nombre de critères, centrées cette fois plutôt sur le système. 
+        Malgré tout, que ce soit la version courte ou longue de l’UEQ, comme pour l’AttrakDiff, le vocabulaire n’est pas accessible aux 7-14 ans. Également, les items et le système d’évaluation peuvent paraître abstraits pour des enfants de moins de 10 ans. 
+        Une aide du même type que celle proposée pour l’AttrakDiff pourrait être intéressante pour rendre plus accessible l’UEQ.
+    </p>
+    <p>
+        En 2019, dans le cadre d’une évaluation d’une application mobile, des chercheurs ont adapté l’échelle aux enfants en rendant le vocabulaire plus accessible <a href = "#Fig8">(Figure 8)</a>. 
+        Le nombre d’items a par contre été conservé.
+    </p>
+
+    <hr ALIGN=CENTER WIDTH="50%"> 
+    <br>
+
+    <h2 id = "$3">Pour conclure</h2> 
+    <br>
+    <p>
+        Ces trois échelles très utilisées sont donc pratiques pour obtenir une évaluation globale d’un produit. 
+        Elles sont par contre plutôt longues et le vocabulaire utilisé n’est pas toujours accessible à tous. 
+        Une piste d’adaptation pour les trois échelles, et déjà exploitée pour d’eux d’entre elles, est de réduire le nombre d’items et simplifier le vocabulaire. 
+        Une autre possibilité est d’imager les items. On en a un exemple grâce à Antonio Giardi avec la figure 15 (p.30).
+    </p>
+
+
+
 
 </body>
 <?php require_once '../includes/footer.php' ?>
