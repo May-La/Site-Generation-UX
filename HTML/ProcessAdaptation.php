04093e8f0cfa828e2e795f7d789bbf2daa20860a
ChloeMonv <cmonvoisi003@ensc.fr>
2023-05-29 22:56:57 +0200

    processu d'adaptation

--- HTML/ProcessAdaptation.php
+++ HTML/ProcessAdaptation.php
@@ -3,10 +3,201 @@
 <?php require_once '../includes/head.php'; ?>
 <?php require_once '../includes/navbar.php'; ?>
 <body>
+<br>
+<H2>Adaptation visuelle et du fonctionnement de l’échelle </H2>
+<br>
+<p>
+Suite à nos recherches lors du semestre 1 pour l'état de l’art, nous avons commencé par adapter le design et le fonctionnement général de l’échelle. 
+</p>
+<p>
+Nous avons ,d’abord, réduit le nombre de choix de réponse passant de 7 à 5 suivant les recommandations de  Borgers et Hox (2000) (page 35 de l’état de l’art). Nous avons donc supprimé 2 des 7 options : “plutôt pas d’accord” et “plutôt d’accord” car ce sont les deux propositions les plus vagues. 
+</p>
+<p>
+En plus des indications textuelles pour les réponses ( “pas du tout d’accord”, “pas d’accord”, “ ni d’accord ni pas d’accord”, ”d’accord”, ”tout à fait d’accord”) nous avons ajoutés des pouces vers le haut et vers le bas pour faciliter la compréhension des réponses possibles. Ces images sont colorées en vert et rouge car ces deux couleurs sont associées à la notion de bien / mal (d’accord/ pas d’accord) mais nous avons quand même fait attention à ce que ces images soient compréhensibles par des enfants daltoniens. Il n’y a donc pas d’information donnée uniquement par la couleur.
+</p>
+
+<img class= "figure" src="../IMG/adapt1.png" alt="Possibilités de réponses originielles ">
+<p class="middle">Possibilités de réponses originielles </p>
 
 
+<img class= "figure" src="../IMG/adapt2.png" alt="Possibilités de réponse adaptées  ">
+<p class="middle">Possibilités de réponse adaptées </p>
+
+<br>
+<p>
+Nous avons aussi remplacé la police d’écriture par une plus facile à lire pour les dyslexiques (ici Opendys) comme indiqué dans notre cahier des charges afin d’augmenter l'accessibilité de l’échelle) . De plus, pour faciliter le remplissage de l’échelle nous avons mis en valeur une phrase sur deux pour que les enfants puissent facilement voir ou répondre pour chaque phrase. 
+</p>	
+<p>
+Dans l’échelle meCue originelle, le module 4 est une unique échelle allant  de -5 à 5 (de bon à mauvais) et répondant à une seule question. Nous avons remplacé cette échelle par cinq émojis allant d’un smiley triste a un très content pour encore une fois limiter les options ( 5 maximum selon Borgers et Hox). 
+</p>
 
 
+<img class= "figure" src="../IMG/adapt3.png" alt="Module 4 de meCue ">
+<p class="middle">Module 4 de meCue </p>
+
+<img class= "figure" src="../IMG/adapt4.png" alt="Module 4 adapté ">
+<p class="middle">Module 4 adapté </p>
+
+<br>
+<p>
+Nous avons aussi choisi de réduire le nombre d’items de l’échelle afin de ne pas avoir une échelle qui prendrait trop de temps à remplir, nous avons alors supprimé les items qui semblaient répétitifs.Par exemple la différence entre “Les fonctionnalités du produit sont parfaitement adaptées à mes objectifs.” et “À l'aide de ce produit, je peux atteindre mes objectifs.” ne paraissait pas significative une fois adapté : “Je pense que ce produit est parfait pour atteindre mes objectifs” et “je peux atteindre mes objectifs grâce à ce produit”. 
+</p>
+
+<H2>Adaptation des items </H2>
+
+<p>
+
+Selon l’état de l’art voici les informations que nous avons pris en compte pour adapter les phrases de meCue: 
+    <ul>
+    <li>Eviter les questions sous forme négative ( Borgers et al, 2000) </li>
+    <li>Éviter l’utilisation de questions hypothétiques ou des questions à double visée ( De leeuw et al. , 2004)
+    <li>Passer les phrases à la première personne  </li>
+    <li>Selon Laufer (1994) éviter le vocabulaire qui présente </li>
+        <ul>
+            <li>une multiplicité de sens</li>
+            <li>des métaphores</li>
+            <li>des idiotismes</li>
+        </ul>
+    </li>
+</ul>
+ 
+
+</p>
+<p>
+Nous avons donc commencé à adapter les phrases qui ne correspondaient pas à ces indications mais très vite nous nous sommes rendus compte que nous ne savions pas si certains mots étaient compréhensibles ou non par des enfants de primaire. 
+</p>
+<p>
+Alors, pour avoir un avis extérieur nous avons réalisé un questionnaire (disponible en annexe) pour des enseignants afin de recueillir leurs opinions sur l’échelle originale. Pour chaque phrase nous demandions si, selon eux, la phrase est compréhensible par un enfant de primaire puis une suggestion de modification s’ils en avaient une. Globalement les phrases que nous avions modifiées sont celles que les enseignants ont indiquées comme problématique. Certaines de leurs suggestions , quand elles étaient rédigées entièrement, ont été utilisées dans l’échelle adaptée car elles paraissaient comme les plus logiques. Pour d’autres suggestions nous n’avons seulement pris en compte les modifications de vocabulaire car la forme ne correspondait pas aux indications nommées précédentes. De  plus, tous les enseignants sont des enseignants de primaire, ils ont donc une idée plus précise de ce qu’un enfant de primaire comprend et ne comprend pas que nous.
+</p>
+<p>
+Le tableau ci-dessous indique les types de modification faites pour chaque phrase dans l’échelle finale.
+</p>
+<ul>
+    <li>VOC = modification du vocabulaire </li>
+    <li>JE = passage à la première personne </li>
+    <li>RAC= phrase raccourcie </li>
+    <li>PROF = suggestion d’un enseignant </li>
+    <li>/  = pas de modification </li>
+</ul>
+
+
+<table>
+    <tr class= "titrestable">
+        <td>Phrase originales</td>
+        <td>Phrases modifiées</td>
+        <td>Type de modification</td>
+    </tr>
+    <tr>
+        <td>MODULE 1</td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>Le produit est facile à utiliser.</td>
+        <td>J&#39;ai facilement utilisé le produit</td>
+        <td>VOC-JE</td>
+    </tr>
+    <tr>
+        <td>Je considère ce produit comme extrêmement utile.</td>
+        <td>Je trouve ce produit très utile.</td>
+        <td>RAC-VOC</td>
+    </tr>
+    <tr>
+        <td>A l&#39;aide de ce produit, je peux atteindre mes objectifs.</td>
+        <td>Avec ce produit, je peux réussir ce qu&#39;on me demande</td>
+        <td>VOC-PROF</td>
+    </tr>
+    <tr>
+        <td>Le produit est conçu de manière créative.</td>
+        <td>j’ai trouvé le produit original.</td>
+        <td>VOC-JE</td>
+    </tr>
+    <tr>
+        <td>Le design a l&#39;air attrayant.</td>
+        <td>j’ai envie d’utiliser ce produit parce qu’il est joli.</td>
+        <td>VOC-JE</td>
+    </tr>
+    <tr>
+        <td>Le produit me donne une meilleure image auprès des autres.</td>
+        <td>Je pense que les autres me voient différemment quand j’utilise ce produit.</td>
+        <td>VOC-JE</td>
+    </tr>
+    <tr>
+        <td>Mes amis peuvent bien être envieux de ce produit.</td>
+        <td>Mes amis voudraient être à ma place quand j’utilise ce produit.</td>
+        <td>VOC-JE</td>
+    </tr>
+    <tr>
+        <td>Le produit est comme un ami pour moi.</td>
+        <td>Ce produit est comme un ami pour moi.</td>
+        <td>VOC</td>
+    </tr>
+    <tr>
+        <td>Si je perdais le produit, j&#39;en serai dévasté.</td>
+        <td>Si je perdais ce produit, je serais très triste.</td>
+        <td>VOC-PROF</td>
+    </tr>
+    <tr>
+        <td>MODULE 2</td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>Le produit m&#39;enthousiasme.</td>
+        <td>J&#39;aime le produit</td>
+        <td>VOC-JE</td>
+    </tr>
+    <tr>
+        <td>Le produit m&#39;apaise</td>
+        <td>Quand j&#39;utilise ce produit, je suis calme</td>
+        <td>VOC-JE</td>
+    </tr>
+    <tr>
+        <td>Quand j’utilise ce produit, je me sens joyeux.</td>
+        <td>Quand j’utilise ce produit, je me sens joyeux</td>
+        <td>/</td>
+    </tr>
+    <tr>
+        <td>Le produit m&#39;énerve.</td>
+        <td>Quand j&#39;utilise ce produit, je m&#39;énerve</td>
+        <td>JE</td>
+    </tr>
+    <tr>
+        <td>Le produit me fatigue.</td>
+        <td>Quand j&#39;utilise ce produit, je me fatigue</td>
+        <td>VOC-JE</td>
+    </tr>
+    <tr>
+        <td>Le produit m’ennuie.</td>
+        <td>Quand j&#39;utilise ce produit, je m’ennuie</td>
+        <td>VOC-JE</td>
+    </tr>
+    <tr>
+        <td>MODULE 3</td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>Je n&#39;échangerais le produit contre aucun autre.</td>
+        <td>Je n’échangerais pas ce produit contre un autre.</td>
+        <td>VOC</td>
+    </tr>
+    <tr>
+        <td>Je n&#39;hésiterais pas à choisir ce produit (à nouveau).</td>
+        <td>Je choisirai ce produit à nouveau.</td>
+        <td>RAC-VOC</td>
+    </tr>
+    <tr>
+        <td>Si je le pouvais, j&#39;utiliserais le produit chaque jour</td>
+        <td>J’ai envie d’utiliser ce produit tous les jours.</td>
+        <td>VOC</td>
+    </tr>
+    <tr>
+        <td>Quand j&#39;utilise ce produit, il m&#39;arrive de perdre la notion du temps.</td>
+        <td>Quand j’utilise ce produit, le temps passe vite.</td>
+        <td>RAC-VOC</td>
+    </tr>
+</table>
 
 </body>
 <?php require_once '../includes/footer.php' ?>
