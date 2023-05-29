46c52b625bfdf6f8367c333c6b72bc86a6174541
ChloeMonv <cmonvoisi003@ensc.fr>
2023-05-29 23:57:36 +0200

    derouelemnt des tests

--- HTML/DetailsTest.php
+++ HTML/DetailsTest.php
@@ -3,9 +3,17 @@
 <?php require_once '../includes/head.php'; ?>
 <?php require_once '../includes/navbar.php'; ?>
 <body>
+<br>
+<H2>Déroulemznt des tests</H2>
+<br>
+<p>
+Nous sommes allés faire passer les tests le jeudi 27 avril de 13h30 à 16h30. Nous avons demandé aux 2 instituteurs de former des groupes de 4 enfants (L’enseignante nous a informé que tous les élèves allaient être volontaires donc pour éviter trop de débordement les enseignants ont formé les groupes) avec une différence de genre (au moins 1 fille ou 1 garçon). Ensuite, nous avons fait en sorte que pour chaque passage deux membres du projet supervisent quatre enfants. Nous commencions par nous présenter en expliquant de manière synthétique qui nous étions et le but de notre expérience. Nous insistions sur le fait qu’il n’y avait pas de raison de s'inquiéter, que rien n’était noté et qu’ils pouvaient arrêter ou poser des questions quand ils voulaient. Après cela, nous avons fait un “ice breaker” avec eux, il était question pour chacun de se présenter rapidement en disant son prénom, nom et plat préféré. L’objectif était de les rendre le plus à l’aise possible et que les freins et biais qui peuvent être causés par une appréhension puissent être évités. Ensuite, les enfants pouvaient remplir les labyrinthe avec le moins possible d’interventions de notre part. Une fois que tous les enfants avaient terminé, nous leur demandions de remplir l’échelle en retranscrivant ce qu’ils pensaient du labyrinthe tout en rappelant que nous étions prêts à entendre leurs remarques et questions. Enfin, nous finissions par leur proposer deux focus groupes. Ces derniers se faisaient sur des post-its et les enfants pouvaient noter tout ce qu’il leur venait en tête dessus (d’ailleurs nous avons trouvé des choses étonnantes qui semblaient n’avoir aucun rapport avec le sujet tel que : “mais se que j’ai penser c’est au coca cola”), avant de rassembler les post-its par thème. Le premier focus group était consacré au labyrinthe tandis que le deuxième avait comme sujet l’échelle. 
+</p>
 
+<p>
+Les tests ont duré entre 30 et 50 minutes selon les groupes. Les enfants étaient nombreux à être volontaires et nous avons eu des retours très positifs comme “je vous encourage à continuer” ou encore “génial”. Généralement, les enfants étaient intéressés et concentrés, mais nous avons aussi eu des cas d’enfants plus turbulents, bavards, moins intéressés par l’échelle ou même un enfant un peu frustré par le temps qu’il a mit à réussir le labyrinthe. Mais cela n’a empêché aucun enfant de remplir l’échelle et de participer aux focus groupes. Nous avons aussi eu des cas d’enfants qui ne comprenaient pas vraiment la raison de l'expérience ou ce que l’on cherchait à faire et qui posaient donc beaucoup de questions à ce propos. Mais l'expérience s'est très bien déroulée et nous avons pu recueillir les résultats souhaités. 
 
-
+</p>
 
 
 </body>
