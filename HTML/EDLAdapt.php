cfcde264fbc92626d5c5d7eb0d9a7e0bb74595ee
mlageste <lageste.maylis@gmail.com>
2023-05-29 04:04:03 +0200

    Ajout all adaptation

--- HTML/EDLAdapt.php
+++ HTML/EDLAdapt.php
@@ -3,10 +3,119 @@
 <?php require_once '../includes/head.php'; ?>
 <?php require_once '../includes/navbar.php'; ?>
 <body>
-
-
-
     <br>
+    <h2 id = "$1">Le rapport à l’enfant et à ses capacités propres</h2>
+    <br>
+    <p>
+        Lobjoie Nicolas nous apprend dans un article pour Altics (voir les sources) que, pour les jeunes enfants, l'évaluation de l’état émotionnel et du ressenti de l’enfant se fait souvent par un tiers en fonction des expressions faciales de l’enfant, de ses réactions, son comportement…
+        Néanmoins au-delà de 6 ans il devient possible de laisser l’enfant s’auto-évaluer par le biais d’une échelle ou d’un dialogue.
+        Cependant, il faut faire attention au design en adaptant une échelle pour des enfants, notamment sur le fait que la génération ciblée présente parfois des différences (culturelles, sociales, éducatives…) avec celle qui conçoit l’échelle. 
+        L’échelle ne doit pas heurter ou exclure les enfants, ils doivent se sentir à l’aise en utilisant l’échelle afin d’obtenir de meilleurs résultats.
+    </p>
+    <p>
+        Le public cible étant exclusivement composé d’enfants de 7 à 14 ans, il convient de respecter les facultés propres à ces derniers. <br>
+        A ce stade de développement, le mode de pensée de l’enfant n’est plus seulement égocentrique, il est maintenant capable de réfléchir sur des événements ou des expériences qu’il n’a pas lui-même vécus. 
+        A partir de ses nombreuses expériences vécues, l’enfant va pouvoir imaginer et se mettre à la place des autres, il commence à établir des liens de cause à effet et être davantage conscient des conséquences de certains événements grâce notamment à son propre vécu. 
+        Face à une nouvelle expérience, l’enfant va se baser sur ces notions pour fonder sa réflexion et établir des schémas de pensée. Pour ce qui est de la classification et de la catégorisation, les enfants peuvent réaliser des regroupements plus avancées comme la sériation 
+        (classer une série d’objets suivant une ou plusieurs caractéristiques : taille, poids, couleur) ou l’inférence transitive (établir la relation entre deux objets) ainsi que l’inclusion de classe (reconnaître les liens entre le tout et les parties).
+    </p>
+    <p>
+        Finalement, lors du stade opératoire concret (cette étape du développement de l’enfant introduite par Piaget se situerait entre 7 et 11 ans environ, elle caractérise les premières réflexions logiques ou opérationnelles ce qui va permettre à l’enfant de comprendre et respecter les règles qui lui sont imposées), 
+        le raisonnement transductif (permet au sujet d’associer des idées, lorsqu’il est face à une situation nouvelle, de réagir en fonction des acquis provenant d'événements passés qu’il juge similaires) des enfants évolue et ce dernier développe un raisonnement inductif (permettant de généraliser des idées à partir d'événements concrets) 
+        qui lui permet, à partir d’observations de quelques exemples, de tirer des conclusions générales. 
+        De plus, l’enfant commence à acquérir un raisonnement déductif (utilise l’abstraction, la recombinaison et les connaissances précédemment acquises pour en générer de nouvelles) et arrive à résoudre quelques problèmes. 
+    </p>
+    <p>
+        Toujours d’après Lobjoie Nicolas, il y a quelques points importants à respecter lors du protocole de test chez l’enfant :
+    </p>
+    <ul>
+        <li>Le test doit absolument durer moins d’une heure (et inclure des pauses si le test est long).</li>
+        <li>L’accent doit être mis sur le jeu et l’aspect ludique du jeu afin de maintenir l’attention de l’enfant et créer de l’engagement chez lui.</li>
+        <li>Il faut simplifier les tâches à accomplir au maximum, quitte à ce que l’enfant se fasse aider pour éviter tout blocage de leur côté.</li>
+        <li>Le cadre environnant doit être rassurant, si les enfants ne sont pas à l’aise les tests peuvent être biaisés (pour cela la présence des parents peut représenter un atout, même si cela semble difficile à mettre en place en pratique).</li>
+        <li>Les comportements verbaux et non verbaux révèlent beaucoup du ressenti de l’enfant.</li>
+        <li>Établir une consigne et des questions claires et s’assurer que les enfants les comprennent.</li>
+        <li>N’intervenir qu’en cas de besoin.</li>
+    </ul>
+    <p>Pour les enfants on peut évaluer (ou compléter l’évaluation) l'expérience qu’il ont eu avec un système avec d’autres outils qu’une échelle  :</p>
+    <ul>
+        <li>en se fiant au taux d’abandon (si le système offre cette possibilité).</li>
+        <li>en mettant en place une “heatmap” qui nous renseignera sur les zones où l’enfant a le plus cliqué.</li>
+        <li>en utilisant de l’eye tracking pour avoir accès aux endroits ayant le plus attiré le regard de l’enfant.</li>
+        <li>en filmant/observant la session pour avoir accès aux réactions et expressions de l’enfant pendant qu’il utilise le système.</li>
+    </ul>
+    <br>
+    <hr ALIGN=CENTER WIDTH="50%"> 
+    <br>
+    <h2>Échelles d’évaluation de la douleur</h2>
+    <br>
+    <p>
+        Il existe plusieurs échelles pour évaluer la douleur chez l’enfant, elles sont surtout utilisées dans le cadre médical.
+        On distingue deux types d’échelles distincts :
+    </p>
+    <p>
+        Les échelles d’hétéro-évaluations dont le principe consiste en une observation par un tiers de la douleur ressentie par l’enfant à l’aide des réactions et des expressions de ce dernier. 
+        Ces échelles sont souvent privilégiées chez les enfants en bas âge pour qui l’auto-évaluation est délicate voir impossible. <br>
+        Dans cette catégorie il existe beaucoup d’échelles dont :
+    </p>
+    <ul>
+        <li>
+            l'échelle CHEOPS (Children’s Hospital of Eastern Ontario Pain Scale) (Figure 19) développée pour les enfants entre 1 et 5 ans, il s'agit de l’une des échelles les plus utilisées. L’observateur, qui le plus souvent est un membre du service hospitalier mais qui peut également être un parent, se charge de remplir la grille en fonction des caractéristiques observables sur l’enfant. Cela mène à l’obtention d’un score compris entre 4 et 13 censé quantifier la douleur ressentie par l’enfant.
+        </li>
+        <img src = "#" alt = "" id = "Fig1">
+        <h5>Figure 1. Echelle CHEOPS</h5>
+        <br>
+        <li>
+            l’échelle PIPP (Premature Infant Pain Profile) (Figure 20) pour évaluer la douleur aiguë chez le nouveau-né prématuré (mais également si le nouveau-né est à terme). Comme pour celle présentée précédemment, cette échelle fonctionne grâce à un système de points, avec un score allant de 0 à 21 obtenu à la suite du remplissage de la grille présentée ci-dessous.
+        </li>
+        <img src = "#" alt = "" id = "Fig2">
+        <h5>Figure 2. Echelle PIPP</h5>
+        <br>
+        <li>Il existe encore de nombreuses autres échelles d’hétéro-évaluation (comme par exemple les échelles EVENDOL, EDIN, OPS, DEGR, QDSA, DAN, FLACC…) mais nous n’allons pas détailler chacune d’entre elles.</li>
+    </ul>
+    <p>
+        Les échelles d’auto-évaluation dont le principe consiste en une estimation par le sujet (ici un enfant) de la douleur qu’il ressent. 
+        Pour pouvoir utiliser ce type d’échelle il faut que l’enfant ait déjà atteint un certain âge afin d’être apte de comprendre l’échelle qui lui est proposée et d’estimer correctement ce qu’il ressent.
+    </p>
+    <p>Dans cette catégorie d’échelle on retrouve : </p>
+    <ul>
+        <li>
+            l’échelle de Wong-Baker <a href = "#Fig3">(Figure 3)</a> (et de manière plus générale les échelles de visages constituent les échelles les mieux validées et les plus efficaces auprès des enfants, figure) est destinée aux enfants de plus de 3 ans. 
+            L’enfant dispose d’une palette comportant 5 dessins de visages plus ou moins déformés par la douleur. Il doit alors choisir  le visage qui correspond le mieux à ce qu’il éprouve.
+        </li>
+        <img src = "#" alt = "" id = "Fig3">
+        <h5>Figure 3. Echelle Echelle de Wong-Baker</h5>
+        <br>
+        <li>l’échelle numérique (EN) qui vise à faire choisir un nombre de 0 à 10 quantifiant la douleur perçue par l’enfant. 0 représentant l’absence de douleur et 10 la plus forte douleur possible.</li>
+        <li>
+            l’échelle verbale simple (EVS) qui consiste à demander à l’enfant de qualifier sa douleur à l’aide d’adjectifs simples et choisis préalablement. 
+            Le choix des mots utilisés dans l’échelle doit être adapté au stade de développement de l’enfant. 
+            Bien qu’utilisée en pratique, cette échelle reste néanmoins peu précise et n’est toujours pas validée pour l’instant.
+        </li>
+    </ul>
+    <br>
+    <hr ALIGN=CENTER WIDTH="50%"> 
+    <br>
+    <h2>Echelles d’évaluation de l’anxiété</h2>
+    <br>
+    <p>
+        Les processus de création d’une échelle de mesure de l’anxiété chez l’enfant sont détaillés dans la thèse Elaboration de l’échelle EVAN, 
+        une échelle d’évaluation de l’anxiété avant les soins aux urgences pédiatriques chez les enfants de 3 à 16 ans de Marie Pourchet pour les enfants hospitalisés :
+    </p>
+    <p>
+        Il existe des moyens de quantifier l’anxiété préopératoire chez l’enfant comme par exemple au travers du score “YALE” appelé le score YPAS, puis m-YPAS 
+        (modified Yale Preoperative Anxiety Scale), ce score est obtenu grâce à une hétéroévaluation de l’anxiété pour des enfants âgés d’au moins 5 ans.
+        <br>
+        Pour des enfants un peu plus âgés, l’auto-évaluation devient possible, c’est ce qui est fait avec l’échelle EVA-anxiété convenant aux enfants entre 7 et 16 ans et qui permet d’évaluer l’anxiété péri-opératoire de manière rapide et répétée.
+        <br>
+        De même, l’échelle d’anxiété de Spielberger a été développée initialement pour des enfants de 9 à 12 ans, mais elle a largement été testée pour des enfants ayant entre 5 et 18 ans. 
+        Elle permet de quantifier l’anxiété chez les enfants sans pour autant permettre d’en déterminer la cause.
+    </p>
+    <p>
+    Sinon il existe des “échelles des émotions” pour essayer d’avoir une idée approchée de l’état émotionnel de l’enfant et/ou de son ressenti mais ces échelles ne sont pas fiables et ne sont pas validées.
+    </p>
+    <br>
+    <a href = "#$1">Retour en haut de la page</a>
     <a href = "EDLA.php">Retour au sommaire</a>
 </body>
 <?php require_once '../includes/footer.php' ?>
--- includes/navbar.php
+++ includes/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light " style="background-color:#50C878;">
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color:#50C878;">
         <a class="navbar-brand" href="Accueil.php">
             <img src="../IMG/logo GUUX (1).png" width="" height="50" class="d-inline-block align-top" alt="Génération UX">
 
@@ -23,8 +23,9 @@
                             <a class="dropdown-item" href="Emotionnelle.php">Domaine émotionnel</a>
                             <a class="dropdown-item" href="Utilisabilite.php">Domaine Utilisabilité</a>
                             <a class="dropdown-item" href="UX.php">Domaine UX</a>
-                            <a class="dropdown-item" href="ValidationSuite.php">Processus d'adaptation et de validation d'une échelle</a>
+                            <a class="dropdown-item" href="Validation.php">Processus de validation d'une échelle</a>
                             <a class="dropdown-item" href="Echelles.php">Echelles retenues pour l'adaptation</a>
+                            <a class="dropdown-item" href="TestDemarch.php">Test et démarchage des écoles</a>
                             <a class="dropdown-item" href="Sources.php">Sources de l'état de l'art</a>
                     </div>
                 </li>
