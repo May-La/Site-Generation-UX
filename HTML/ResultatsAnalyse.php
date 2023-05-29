c30b7b30d4b1e6bbe1f0d5c138ea05a981f32fed
ChloeMonv <cmonvoisi003@ensc.fr>
2023-05-29 23:19:31 +0200

    2 sur 7 analyses

--- HTML/ResultatsAnalyse.php
+++ HTML/ResultatsAnalyse.php
@@ -3,9 +3,96 @@
 <?php require_once '../includes/head.php'; ?>
 <?php require_once '../includes/navbar.php'; ?>
 <body>
+    <br>
+<H2>Fidélité de l’échelle</H2>
+<br>
+<p>
+La fidélité d’une échelle s’évalue surtout à l’aide d’outils statistiques. On applique ces derniers aux résultats obtenus à l'issue des passages de test. Plus l’échantillon de personnes effectuant le test est important, plus l’évaluation de la fidélité est fiable. Nous avons fait remplir notre échelle à 28 élèves et avons donc recueilli autant de résultats. Généralement, lors des protocoles de vérification de fidélité d'une échelle, le nombre de sujets est plus important et les expérimentations sont répétées. Néanmoins, l’étude des données que nous avons obtenues font sens et nous permettent déjà d’aboutir à des conclusions.
+</p>
+<p>
+Pour vérifier la cohérence intra module nous avons utilisé le coefficient alpha de Chronbach. Ce coefficient est utilisé en recherche pour estimer la fidélité d’une échelle, sa valeur est comprise entre 0 et 1. Plus les réponses aux items que l’on a choisi d’observer sont corrélés, plus sa valeur est élevée. Ainsi, on estime qu’une valeur supérieure à 0,70 est suffisante pour montrer une corrélation. Nous avons décidé de calculer le coefficient (noté ⍺) pour chacun des modules car ces derniers mesurent des choses différentes. Nous l’avons calculé conformément à la formule suivante : 
+</p>
+<p class="middle">
+⍺ = (k/(k-1))*(1-(Si/St))
+</p>
+<ul>
+<li>k est le nombre d’items dans le module.</li>
+<li>Si est la somme des variances des scores obtenues pour chaque item.</li>
+<li>St est la variance des sommes des scores obtenues par chaque participant.</li>
+</ul>
+<p>
+Ainsi nous avons obtenu une valeur légèrement supérieure à 0,77 pour le premier module, une valeur d’environ 0,50 pour le second et enfin la valeur correspondant au dernier module est d’environ 0,45. 
+</p>
+<p>
+Ces valeurs reflètent une bonne corrélation entre les items du premier module mais une corrélation bien moins importante pour les deux suivants. 
+</p>
 
+<hr>
 
+<H2>Validité de l’échelle</H2>
+<br>
+<p>
+S’assurer de la validité d’une d’échelle revient à contrôler que cette dernière quantifie ce que l’on souhaite. Dans notre cas de figure, nous avons adapté une échelle et la vérification de la fidélité va donc consister à veiller à ce que notre échelle soit le plus fidèle possible à meCue (l’échelle originale). La difficulté principale était que meCue n’est pas conçu pour les enfants et donc que nous ne pouvions pas la faire remplir à des enfants afin de comparer ces résultats avec ceux obtenus avec notre échelle. Cependant, nous avons tout de même fait notre possible pour maximiser la validité de notre échelle qui repose principalement sur deux axes.
+</p>
+<p>
+Premièrement, lors de l’adaptation, nous avons cherché à minimiser les différences avec meCue sur le sens et la formulation des phrases. Cela s’est manifesté en pratique par des échanges directs et au travers de questionnaires avec des enseignants. Cette démarche avait pour objectif de recueillir leurs avis sur le vocabulaire à utiliser et les modifications générales à apporter tout en insistant sur le fait qu’il était essentiel de déformer le moins possible l’échelle originale. Ces interactions avec les experts que représentent les enseignants ont pour objectif d’améliorer autant que possible la proximité avec meCue.
+</p>
+<p>
+Deuxièmement, l’échelle adaptée fournit des résultats cohérents. Nous avons constaté que lors des échanges avec les enfants et lors du focus group sur le labyrinthe que des remarques ayant déjà été prises en compte par l’échelle étaient formulées. Cela indiquerait que les questions de l’échelle sont pertinentes et permettent de retranscrire le ressenti des enfants sur le labyrinthe. De plus, les scores moyens des labyrinthes étaient de 22,77 pour le labyrinthe facile, 23,42 pour celui sur l’espace et 23,63 pour le labyrinthe en forme de citrouille. Ces résultats sont rassurants car ils s’accordent avec les dires des enfants (quelques remarques d’enfants soulignent le fait que le simple labyrinthe était trop facile). Mais ils concordent aussi très bien avec les résultats obtenus au quatrième module.
+</p>
+<p>
+Voici l’analyse détaillée de certains résultats significatifs(on retrouve l’effectif de chaque réponses) : 
+</p>
+<br>
+<h6>J’ai facilement utilisé ce produit : </h6>
 
+<div class="troisfigures">
+    <img class= "figuretrois" src="../IMG/facil1.png" alt="Possibilités de réponses originielles ">
+    <img class= "figuretrois" src="../IMG/facil2.png" alt="Possibilités de réponses originielles ">
+    <img class= "figuretrois" src="../IMG/facil3.png" alt="Possibilités de réponses originielles ">
+</div>
+<br>
+<p>
+On remarque que le labyrinthe “simple” semble avoir été plus facile d'utilisation selon les réponses à l'échelle: 5/12 tout à fait d’accord, et 4/12 d’accord ce qui fait 9/12 c’est à dire 75% des enfants ayant une réponse positive. De plus, il n'y a pas de réponses négatives ( d’accord ou pas d’accord), seulement un enfant neutre.
+</p>
+<p>
+Cependant il y a 2 élèves qui n’ont pas à la question. Ceci peut s’expliquer par le fait que la question était la première sur la page et était trop proche des consignes en haut. 
+</p>
+<p>
+On peut aussi observer que les labyrinthes “espace” et “citrouille” ont reçu eu des résultats négatifs ( ⅛ des enfant pour chaque labyrinthe) ce qui s’explique par la complexité plus élevées des labyrinthe : 
+    </p>
+<p>
+ Les réponses à cette question  corrèlent avec le temps passé en moyenne sur les labyrinthes ( 51,5 secondes pour “simple” ) . “Citrouille” ayant pris en moyenne 146 secondes pour être complété et “espace” 104 secondes. 
+ </p>
+ <p>
+ On peut en déduire que les réponses à cette question ont été correctement transmises par notre échelle.
+ </p>
+ <p>
+ Il faut cependant noter que les moyennes en secondes et les réponses nous indiquent que notre hypothèse de base qui était que “citrouille” est le plus compliqué semble fausse au vu des comparaison avec “espace” (141 secondes en moyenne pour espace > 105 secondes en moyenne pour citrouille). Il y a plus d’enfants qui ont dit que le labyrinthe citrouille était facile que le labyrinthe espace (⅝ et 4/8) 
+ </p>
+
+ <br>
+<h6>J'ai envie d'utiliser ce produit parce qu'il est joli</h6>
+
+<div class="troisfigures">
+    <img class= "figuretrois" src="../IMG/joli1.png" alt="Possibilités de réponses originielles ">
+    <img class= "figuretrois" src="../IMG/joli2.png" alt="Possibilités de réponses originielles ">
+    <img class= "figuretrois" src="../IMG/joli3.png" alt="Possibilités de réponses originielles ">
+</div>
+<br>
+
+<p>
+On observe que indépendamment du type de labyrinthe, l’échelle laisse penser que les enfants n’ont pas envie d’utiliser le labyrinthe parce que le produit est joli. En effet, on observe en majorité des réponses neutres, surtout pour le labyrinthe simple, ce qui est logique puisqu’il n’est pas spécialement joli. 
+</p>
+<p>
+Donc d’après l’échelle, le graphisme du labyrinthe importe peu sur l’envie d’y jouer.
+</p>
+<p>
+En discutant avec les participants après le passage du test, on a pu entendre que les enfants avaient envie de faire ces labyrinthes, non pas parce qu’ils étaient jolis mais plutôt parce que le concept de labyrinthe est amusant en général. 
+</p>
+<p>
+On peut en déduire que les réponses à cette question ont été correctement transmises par notre échelle, mais que à travers la simple utilisation de l’échelle, on ne peut pas avoir d’information sur pourquoi ils ont envie d'utiliser le produit.
+</p>
 
 
 </body>
